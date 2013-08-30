d35a1a300d9e27d090df6a90b2e8f7d1a1879496
David Dollar <ddollar@gmail.com>
2013-08-29 20:54:59 -0400

    windows

---
+++ windows.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "os/exec"
+  "syscall"
+)
+
+func (p *Process) Start() {
+	command := []string{"cmd", "/C", p.Command}
+	p.cmd = exec.Command(command[0], command[1:]...)
+	p.cmd.Dir = p.Root
+	p.cmd.Env = p.envAsArray()
+	p.cmd.Stdin = p.Stdin
+	p.cmd.Stdout = p.Stdout
+	p.cmd.Stderr = p.Stderr
+	p.cmd.Start()
+}
+
+func (p *Process) Signal(signal syscall.Signal) {
+	group, _ := os.FindProcess(-1 * p.cmd.Process.Pid)
+	group.Signal(signal)
+}
+
+func ShutdownProcesses(of *OutletFactory) {
+	shutdown_mutex.Lock()
+	of.SystemOutput("shutting down")
+	for name, ps := range processes {
+		of.SystemOutput(fmt.Sprintf("terminating %s", name))
+		ps.cmd.Process.Signal(os.Kill)
+	}
+	os.Exit(1)
+}
