c5aa0cd0e1ff72579e9363b2fc351f54aba019c1
David Dollar <ddollar@gmail.com>
2013-09-17 19:23:41 -0400

    fix error in linux build

--- linux.go
+++ linux.go
@@ -10,7 +10,7 @@ import (
 )
 
 func (p *Process) Start() {
-	command := []string{"/bin/bash", p.bashArgument(), fmt.Sprintf("source \"%s\" 2>/dev/null; %s", filepath.Join(p.Root, ".profile"), p.Command)}
+	command := []string{"/bin/bash", p.shellArgument(), fmt.Sprintf("source \"%s\" 2>/dev/null; %s", filepath.Join(p.Root, ".profile"), p.Command)}
 	p.cmd = exec.Command(command[0], command[1:]...)
 	p.cmd.Dir = p.Root
 	p.cmd.Env = p.envAsArray()
