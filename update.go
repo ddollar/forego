6e983fe41c7dbb4fcb4437190094530fbfbaaf84
David Dollar <ddollar@gmail.com>
2013-09-22 16:19:30 -0400

    remove optional version from update

--- update.go
+++ update.go
@@ -7,7 +7,7 @@ import (
 
 var cmdUpdate = &Command{
 	Run:   runUpdate,
-	Usage: "update [version]",
+	Usage: "update",
 	Short: "Update forego",
 	Long: `
 Update forego
@@ -15,7 +15,6 @@ Update forego
 Examples:
 
 	forego update
-	forego update 0.7.8
 `,
 }
 
@@ -28,14 +27,7 @@ func runUpdate(cmd *Command, args []string) {
 		return
 	}
 	d := dist.NewDist("ddollar/forego", Version)
-	var err error
-	var to string
-	if len(args) > 0 {
-		err = d.UpdateTo(args[0])
-		to = args[0]
-	} else {
-		to, err = d.Update()
-	}
+	to, err := d.Update()
 	if err != nil {
 		fmt.Printf("ERROR: %s\n", err)
 	} else {
