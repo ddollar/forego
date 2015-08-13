796660ff0d72e6baae48dd243d8b6a88732f4f14
Josh Mervine <joshua@mervine.net>
2015-08-13 15:38:00 -0700

    start concurrency only runs listed processes

--- start.go
+++ start.go
@@ -243,8 +243,12 @@ func runStart(cmd *Command, args []string) {
 
 	for idx, proc := range pf.Entries {
 		numProcs := defaultConcurrency
-		if value, ok := concurrency[proc.Name]; ok {
-			numProcs = value
+		if len(concurrency) > 0 {
+			if value, ok := concurrency[proc.Name]; ok {
+				numProcs = value
+			} else {
+				continue
+			}
 		}
 		for i := 0; i < numProcs; i++ {
 			if (singleton == "") || (singleton == proc.Name) {
