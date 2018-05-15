402f02ad3e120f385fcf41ed1cc1f71ace350347
Sebastian Schwarz <schwarz@big5.de>
2018-05-15 14:30:37 +0200

    Use /bin/sh instead of bash
    There are plenty of environments without BASH, but with sh.

--- unix.go
+++ unix.go
@@ -14,8 +14,8 @@ func ShellInvocationCommand(interactive bool, root, command string) []string {
 	if interactive {
 		shellArgument = "-ic"
 	}
-	shellCommand := fmt.Sprintf("cd \"%s\"; source .profile 2>/dev/null; exec %s", root, command)
-	return []string{"sh", shellArgument, shellCommand}
+	shellCommand := fmt.Sprintf("cd \"%s\"; . ./.profile 2>/dev/null; exec %s", root, command)
+	return []string{"/bin/sh", shellArgument, shellCommand}
 }
 
 func (p *Process) PlatformSpecificInit() {
